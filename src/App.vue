145d94749d7ccf7c72a05d7c189c8f385960c8da
HP <rufathasanov1998@gmail.com>
2020-11-25 20:20:14 +0500

    neee

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,102 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+  <v-navigation-drawer 
+  app 
+  v-model="drawer"
+  temporary
+  absolute>
+    <!-- -->
+
+      <v-list-item @click="drawer = !drawer" class="text-accent-4">
+        <v-list-item-icon>
+          <v-icon>mdi-text</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>App</v-list-item-title>
+      </v-list-item>
+      
+      <v-btn text flat
+      v-for="link of links"
+      :key="link.title"
+      :to="link.url"
+      >
+        <v-icon left>
+          {{ link.icon }}
+        </v-icon>
+        {{ link.title }}
+      </v-btn>
+  </v-navigation-drawer>
+<!--Nav drawer-->
+
+<!--Toolbar-->
+  <v-card
+    flat
+    tile
+  >
+    <v-toolbar dark color="blue">
+      <v-app-bar-nav-icon 
+      @click="drawer = !drawer"
+      class="hidden-md-and-up"
+      >
+      </v-app-bar-nav-icon>
+
+      <v-toolbar-title>App</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items dark color="blue" class="hidden-sm-and-down">
+      <v-btn text flat
+      v-for="link of links"
+      :key="link.title"
+      :to="link.url"
+      >
+        <v-icon left>
+          {{ link.icon }}
+        </v-icon>
+        {{ link.title }}
+      </v-btn>
+      </v-toolbar-items>
+
+    </v-toolbar>
+  </v-card>
+    <!--Toolbar-->
+
+
+
+  <!-- Sizes your content based upon application components -->
+  <v-main>
+
+    <!-- Provides the application the proper gutter -->
+    <v-container fluid>
+
+      <!-- If using vue-router -->
+      <router-view></router-view>
+    </v-container>
+  </v-main>
+
+
+
+  <v-footer app>
+    <!-- -->
+  </v-footer>
+</v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      drawer: false,
+      links: [
+        {title:'Login', icon:'mdi-lock', url:'/login'},
+        {title:'Registration', icon:'mdi-account', url:'/registration'},
+        {title:'Orders', icon:'mdi-bookmark', url:'/orders'},
+        {title:'New add', icon:'mdi-note', url:'/new'},
+        {title:'My adds', icon:'mdi-book', url:'/list'},
+      ]
+    }
   }
-}
-</script>
+ 
+  }
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+</script>
