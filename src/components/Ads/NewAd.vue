1598a73c0a7e7d8b62ac209296ac41cfc4bb46e3
Rufat Xasanov <you@example.com>
2021-01-11 17:11:44 +0500

    Newad da funksiyalar yaratildi

--- src/components/Ads/NewAd.vue
+++ src/components/Ads/NewAd.vue
@@ -1,8 +1,72 @@
 <template>
   <v-container>
       <v-layout row>
-          <v-flex xs12>
-             <h1>NewAd</h1>
+          <v-flex xs12 sm6 offset-sm3>
+             <h1 class="text--secondary mb-3">Create new add</h1>
+             <v-form v-model="valid" ref="form" validation class="mb-3">
+
+                  <v-text-field 
+                  prepend-icon="mdi-account" 
+                  name="title" 
+                  label="Ad title" 
+                  type="text"
+                  v-model="title"
+                  required 
+                  :rules="[v => !!v || 'Title is required']"
+                  >
+                  </v-text-field>
+
+                  <v-text-field 
+                  prepend-icon="mdi-lock" 
+                  name="description" 
+                  label="Ad description" 
+                  type="text" 
+                  v-model="description"
+                  multi-line
+                  :rules="[v => !!v || 'Description is required']"
+                  >
+                  </v-text-field>
+                </v-form>
+
+                <v-layout row class="mb-3">
+                    <v-flex xs12>
+                        <v-btn class="warning">
+                            Upload
+                            <v-icon right dark>mdi-upload</v-icon>
+                        </v-btn>
+                    </v-flex>
+                </v-layout>
+
+                <v-layout row>
+                    <v-flex xs12>
+                        <img src="" height="100" alt="">
+                    </v-flex>
+                </v-layout>
+                
+                <v-layout row>
+                    <v-flex xs12>
+                        <v-switch
+                        label="Ad to promo?"
+                        v-model="promo"
+                        >
+                        </v-switch>
+                    </v-flex>
+                </v-layout>
+
+                <v-layout row>
+                    <v-flex xs12>
+                        <v-spacer>
+
+                        </v-spacer>
+                        <v-btn
+                        :disabled="!valid"
+                        class="success"
+                        @click="createAd"
+                        >
+                            Create Ad
+                        </v-btn>
+                    </v-flex>
+                </v-layout>
           </v-flex>
       </v-layout>
   </v-container>
@@ -12,7 +76,24 @@
 export default {
     data () {
         return {
+            title: '',
+            description: '',
+            promo: false,
+            valid: false
+        }
+    },
+    methods: {
+        createAd () {
+            if (this.$refs.form.validate()) {
+                //logic
+                const ad = {
+                    title: this.title,
+                    description: this.description,
+                    promo: this.promo
+                }
 
+                console.log(ad)
+            }  
         }
     }
 }
